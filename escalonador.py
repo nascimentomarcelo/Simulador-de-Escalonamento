e1a5c032002bc306746a164540cce17e20a1d68f
Marcelo <nascimentomarcelo589@gmail.com>
2024-09-17 16:44:27 -0300

    up: correção na logica de calculo de creditos, e remoção de criterio de desempate

--- escalonador.py
+++ escalonador.py
@@ -21,7 +21,9 @@ class Processo:
         self.tempo_bloqueado_restante = 0
         self.tempo_cpu_restante = tempo_total_cpu
         self.tempo_execucao = 0
-        self.em_burst = False
+    
+    def __str__(self) -> str:
+        return ", ".join([self.nome, self.estado])
 
 class Escalonador:
     def __init__(self, processos):
@@ -36,28 +38,43 @@ class Escalonador:
             self.imprimir_linha_do_tempo()
             self.atualizar_estados_processos()
 
-            if all(p.creditos == 0 for p in self.processos):
+            if all(p.creditos == 0 or p.estado == EstadoProcesso.BLOCKED for p in self.processos):
                 self.redistribuir_creditos()
+                if not self.processo_executando is None:
+                    self.processo_executando.estado = EstadoProcesso.READY
+            
+            #if self.processo_executando is None or self.processo_executando.creditos == 0 or self.processo_executando.estado == EstadoProcesso.BLOCKED:
+            #    self.proximo_processo = self.obter_proximo_processo()
 
-            if self.processo_executando is None or self.processo_executando.creditos == 0 or self.processo_executando.estado == EstadoProcesso.BLOCKED:
-                self.proximo_processo = self.obter_proximo_processo()
-
+            if (self.processo_executando is None) or (self.processo_executando.creditos == 0 or self.processo_executando.estado == EstadoProcesso.BLOCKED or self.processo_executando.estado == EstadoProcesso.READY): 
+                self.proximo_processo = self.obter_proximo_processo(self.processo_executando)
+                
             if self.proximo_processo:
                 self.executar_processo(self.proximo_processo)
-
+                
             self.tempo_atual += 1
 
     def todos_processos_concluidos(self):
         return all(p.estado == EstadoProcesso.EXIT for p in self.processos)
 
-    def obter_proximo_processo(self):
+    def obter_proximo_processo(self, atual):
         proximo_processo = None
-        for processo in self.processos:
-            if processo.estado not in [EstadoProcesso.BLOCKED, EstadoProcesso.EXIT]:
+
+        if not atual is None:
+            indice_acessado = self.processos.index(atual)
+            if indice_acessado + 1  >= len(self.processos):
+                ordem_processos = self.processos
+            else:
+                ordem_processos = self.processos[indice_acessado+1:] + self.processos[:indice_acessado+1]
+        else:
+            ordem_processos = self.processos
+        
+        for processo in ordem_processos:
+            if not processo.estado in [EstadoProcesso.BLOCKED, EstadoProcesso.EXIT]:
                 if proximo_processo is None or (processo.creditos > proximo_processo.creditos and processo != self.ultimo_processo):
                     proximo_processo = processo
-                elif (processo.creditos == proximo_processo.creditos and processo.ordem < proximo_processo.ordem and processo != self.ultimo_processo):
-                    proximo_processo = processo
+                #elif (processo.creditos == proximo_processo.creditos and processo.ordem < proximo_processo.ordem and processo != self.ultimo_processo):
+                #    proximo_processo = processo
         self.ultimo_processo = self.processo_executando
         return proximo_processo
 
@@ -75,22 +92,24 @@ class Escalonador:
         processo.tempo_execucao += 1
 
         if processo.tempo_cpu_restante == 0:
+            # adiciona uma mensagem de quando cada processo acaba, pois eles não vão continuar aparecendo 
+            print(f"Processo {processo.nome} EXIT no tempo {self.tempo_atual + 2} ms. Créditos no tempo anterior: {processo.creditos}\n") 
             processo.estado = EstadoProcesso.EXIT
             processo.tempo_conclusao = self.tempo_atual
+            processo.creditos = 0
             self.processo_executando = None
         elif processo.tempo_execucao == processo.cpu_burst:
             processo.estado = EstadoProcesso.BLOCKED
             processo.tempo_bloqueado_restante = processo.duracao_io
             self.processo_executando = None
 
+
     def redistribuir_creditos(self):
         for processo in self.processos:
             if processo.estado != EstadoProcesso.EXIT:
-                processo.creditos = (processo.creditos // 2) + processo.prioridade
-            if processo.estado == EstadoProcesso.BLOCKED and processo.tempo_bloqueado_restante == 0:
-                processo.estado = EstadoProcesso.READY
-        self.ultimo_processo = None
-        self.processo_executando = None
+                processo.creditos = (processo.creditos//2) + processo.prioridade
+        self.ultimo_processo = self.processo_executando
+        #self.processo_executando = None
 
     def atualizar_estados_processos(self):
         for processo in self.processos:
@@ -99,6 +118,8 @@ class Escalonador:
                 if processo.tempo_bloqueado_restante == 0:
                     processo.estado = EstadoProcesso.READY
                     processo.tempo_execucao = 0
+            if processo.estado == EstadoProcesso.RUNNING and processo.creditos == 0: # adicionado após o Processo A continuar como running no tempo 12 ms
+                processo.estado = EstadoProcesso.READY
 
     def imprimir_linha_do_tempo(self):
         print(f"Tempo: {self.tempo_atual} ms")
@@ -107,6 +128,7 @@ class Escalonador:
                 print(f"{processo.nome} ({processo.estado} - crédito = {processo.creditos})")
         print()
 
+# exemplo de escalonamento disponivel 
 processos = [
     Processo("A", 2, 5, 6, 1, 3),
     Processo("B", 3, 10, 6, 2, 3),
